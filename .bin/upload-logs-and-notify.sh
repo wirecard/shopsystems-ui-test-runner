9249b72acf484e84e49ffbff152bbc674d9b2482
brkicadis <adis.brkic@wirecard.com>
2020-06-08 08:21:18 +0200

    Add woocommerce and generic files

---
+++ .bin/upload-logs-and-notify.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+# This script will send the notification if 'fail' parameter is passed it will
+set -e # Exit with nonzero exit code if anything fails
+export REPO_NAME='reports'
+export REPO_LINK="https://github.com/wirecard/${REPO_NAME}"
+export REPO_ADDRESS="${REPO_LINK}.git"
+
+echo "Timestamp : $(date)"
+RANDOM_VALUE=$[ ( RANDOM % 30 ) * ${WOOCOMMERCE_NUMBER} + 1 ]
+echo "Sleeping for: ${RANDOM_VALUE}"
+sleep ${RANDOM_VALUE}s
+
+git clone ${REPO_ADDRESS}
+export TODAY=$(date +%Y-%m-%d)
+
+export PROJECT_FOLDER="${SHOP_SYSTEM_NAME}-${SHOP_SYSTEM_VERSION}"
+if [ ! -d "${REPO_NAME}/${PROJECT_FOLDER}/${GATEWAY}" ]; then
+mkdir -p ${REPO_NAME}/${PROJECT_FOLDER}/${GATEWAY}
+fi
+
+if [ ! -d "${REPO_NAME}/${PROJECT_FOLDER}/${GATEWAY}/${TODAY}" ]; then
+mkdir ${REPO_NAME}/${PROJECT_FOLDER}/${GATEWAY}/${TODAY}
+fi
+
+export BRANCH_FOLDER=${GIT_BRANCH}
+export RELATIVE_REPORTS_LOCATION=${PROJECT_FOLDER}/${GATEWAY}/${TODAY}/${BRANCH_FOLDER}
+
+if [ ! -d "${REPO_NAME}/${RELATIVE_REPORTS_LOCATION}" ]; then
+    mkdir ${REPO_NAME}/${RELATIVE_REPORTS_LOCATION}
+fi
+
+export SHOP_SYSTEM=${SHOP_SYSTEM_NAME%-*}
+
+HTML_REPORT=$(jq -r ".$SHOP_SYSTEM.html" extension-shop-system-builder/configuration.json)
+XML_REPORT=$(jq -r ".$SHOP_SYSTEM.xml" extension-shop-system-builder/configuration.json)
+PNG_REPORT=$(jq -r ".$SHOP_SYSTEM.png" extension-shop-system-builder/configuration.json)
+
+cp ${HTML_REPORT} ${REPO_NAME}/${RELATIVE_REPORTS_LOCATION}
+cp ${XML_REPORT} ${REPO_NAME}/${RELATIVE_REPORTS_LOCATION}
+if [[ $1 == 'fail' ]]; then
+    cp ${PNG_REPORT} ${REPO_NAME}/${RELATIVE_REPORTS_LOCATION}
+fi
+
+cd ${REPO_NAME}
+
+git add ${PROJECT_FOLDER}/${GATEWAY}/${TODAY}/*
+git commit -m "Add failed test screenshots from https://github.com/wirecard/${SHOP_SYSTEM_NAME}/actions/runs/${GITHUB_RUN_ID}"
+git push -q https://${GITHUBTOKEN}@github.com/wirecard/${REPO_NAME}.git master
+
+export SCREENSHOT_COMMIT_HASH=$(git rev-parse --verify HEAD)
+if [[ $1 == 'fail' ]]; then
+    cd ..
+    bash extension-shop-system-builder/.bin/send-notify.sh
+fi
---
+++ woocommerce-ee/generate-release-package.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# This script will generate release package and keep composer.json if 'test' parameter is passed
+
+TARGET_DIRECTORY="wirecard-woocommerce-extension"
+
+composer install --no-dev
+
+zip -r extension-shop-system-builder/woocommerce-ee/woocommerce-wirecard-ee.zip ${TARGET_DIRECTORY} -x "*tests*" -x "*Test*" -x "*codeception*"
