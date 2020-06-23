ef404fd18edea4a7e3857652dba6a412a8a091a8
brkicadis <adis.brkic@wirecard.com>
2020-06-23 17:59:27 +0200

    Add robustness check

--- .bin/send-notify.sh
+++ .bin/send-notify.sh
@@ -23,14 +23,28 @@ PNG_REPORT=$(jq -r ".$SHOP_SYSTEM.png" shopsystems-ui-test-runner/configuration.
 
 FAILED_TESTS=$(ls -1q "$PNG_REPORT" | wc -l)
 
-curl -X POST -H 'Content-type: application/json' --data "{
-    'attachments': [
-        {
-            'fallback': 'Failed test data',
-            'text': 'There are failed tests.
-             Test report: ${PREVIEW_LINK}/${SCREENSHOT_COMMIT_HASH}/${RELATIVE_REPORTS_LOCATION}/${REPORT_FILE} .
-             All screenshots can be found  ${REPO_LINK}/tree/${SCREENSHOT_COMMIT_HASH}/${RELATIVE_REPORTS_LOCATION} .',
-            'color': '#fc0000'
-        }
-    ], 'channel': '${CHANNEL}'
-}"  ${SLACK_ROOMS};
+if [ -n "$FEATURE_FILES" ]; then
+  curl -X POST -H 'Content-type: application/json' --data "{
+      'attachments': [
+          {
+              'fallback': 'Failed test data',
+              'text': 'Unstable acceptance tests detected.
+               Test report: ${PREVIEW_LINK}/${SCREENSHOT_COMMIT_HASH}/${RELATIVE_REPORTS_LOCATION}/${REPORT_FILE} .
+               All screenshots can be found  ${REPO_LINK}/tree/${SCREENSHOT_COMMIT_HASH}/${RELATIVE_REPORTS_LOCATION} .',
+              'color': '#fc0000'
+          }
+      ], 'channel': '${CHANNEL}'
+  }"  ${SLACK_ROOMS};
+else
+  curl -X POST -H 'Content-type: application/json' --data "{
+      'attachments': [
+          {
+              'fallback': 'Failed test data',
+              'text': 'There are failed tests.
+               Test report: ${PREVIEW_LINK}/${SCREENSHOT_COMMIT_HASH}/${RELATIVE_REPORTS_LOCATION}/${REPORT_FILE} .
+               All screenshots can be found  ${REPO_LINK}/tree/${SCREENSHOT_COMMIT_HASH}/${RELATIVE_REPORTS_LOCATION} .',
+              'color': '#fc0000'
+          }
+      ], 'channel': '${CHANNEL}'
+  }"  ${SLACK_ROOMS};
+fi
