299c74f00252a078988e64486765168d06700ff6
tatjana.starcenko <tatjana.starcenko@wirecard.com>
2020-06-30 16:26:42 +0200

    Fix bash syntax

--- .bin/run-ui-tests.sh
+++ .bin/run-ui-tests.sh
@@ -93,6 +93,7 @@ else
     -e BROWSERSTACK_ACCESS_KEY="${BROWSERSTACK_ACCESS_KEY}" \
     codecept run acceptance \
     -g "${SHOP_SYSTEM}" -g "${TEST_GROUP}" \
-#     -g "${SHOP_SYSTEM}" -g "${TEST_GROUP}" "${EXCLUDE_TEST_GROUP_FLAG}" \
     --env ci --html --xml
 fi
+
+#     -g "${SHOP_SYSTEM}" -g "${TEST_GROUP}" "${EXCLUDE_TEST_GROUP_FLAG}" \
